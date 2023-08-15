c6c3f3b7f6219512aadc8df23294df598bb515e5
Stepan Poperechnyi <poperechnyi1@gmail.com>
2023-08-15 18:17:00 +0300

    add converting text to JSON

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
+import { getBodyParserOptions } from '@nestjs/platform-express/adapters/utils/get-body-parser-options.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
   await app.listen(3000);
 }
 bootstrap();
--- src/parser/parser.controller.ts
+++ src/parser/parser.controller.ts
@@ -1,9 +1,60 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('parser')
 export class ParserController {
-  @Post()
-  importDumpFrpmJSON(): string {
-    return 'import';
+  @Post('import')
+  async importDumpFromRawText(@Req() request: Request, @Res() res) {
+    const rawBody = await this.getRawBody(request);
+    console.log(9, rawBody);
+    const convertedJSON = this.parseIndentedTextToJSON(rawBody);
+    res.send(convertedJSON);
+  }
+
+  private getRawBody(request: Request): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let rawBody = '';
+      request.on('data', (chunk) => {
+        rawBody += chunk.toString();
+      });
+
+      request.on('end', () => {
+        resolve(rawBody);
+      });
+
+      request.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  private parseIndentedTextToJSON(text) {
+    const lines = text.split('\n');
+    const result = {};
+    const stack = [];
+
+    for (const line of lines) {
+      const indentation = line.search(/\S/);
+      const content = line.trim();
+
+      while (stack.length > indentation) {
+        stack.pop();
+      }
+
+      if (content.includes('=')) {
+        const [key, value] = content.split('=').map((item) => item.trim());
+        stack[stack.length - 1][key] = value;
+      } else {
+        const newObj = {};
+        if (stack.length === 0) {
+          result[content] = newObj;
+        } else {
+          stack[stack.length - 1][content] = newObj;
+        }
+        stack.push(newObj);
+      }
+    }
+
+    return result;
   }
 }
